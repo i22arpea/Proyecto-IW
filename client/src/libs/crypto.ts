d0c4959dfdbbf41473661ac82a8e63b0d29d3548
adriarroyo08 <adriarroyo2002@gmail.com>
2025-05-18 18:26:20 +0200

    Refactor crypto utility functions for improved secret key management

--- client/src/libs/crypto.ts
+++ client/src/libs/crypto.ts
@@ -1,7 +1,19 @@
 import CryptoJS from 'crypto-js';
 
-// Usa la variable de entorno y si no existe, usa una clave por defecto para desarrollo
-const secretKey = process.env.REACT_APP_SECRET_KEY || 'palabrasecreta123';
+// Extiende la interfaz Window para incluir REACT_APP_SECRET_KEY
+declare global {
+  interface Window {
+    REACT_APP_SECRET_KEY?: string;
+  }
+}
+
+function getSecretKey(): string {
+  return (
+    process.env.REACT_APP_SECRET_KEY ||
+    window.REACT_APP_SECRET_KEY ||
+    'default_secret_key'
+  );
+}
 
 export function encriptarPalabra(palabra: string): string {
   const clave = getSecretKey();
@@ -16,19 +28,4 @@ export function desencriptarPalabra(textoCifrado: string): string {
   const textoPlano = bytes.toString(CryptoJS.enc.Utf8);
 
   return textoPlano;
-}
-
-// Extiende la interfaz Window para incluir REACT_APP_SECRET_KEY
-declare global {
-  interface Window {
-    REACT_APP_SECRET_KEY?: string;
-  }
-}
-
-function getSecretKey(): string {
-  return (
-    process.env.REACT_APP_SECRET_KEY ||
-    window.REACT_APP_SECRET_KEY ||
-    'default_secret_key'
-  );
 }
