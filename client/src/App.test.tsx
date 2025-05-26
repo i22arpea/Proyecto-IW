635dec5f8b46e1977f19d2d7b48126eb44dcf983
i22arpea <114402693+i22arpea@users.noreply.github.com>
2025-05-26 15:30:37 +0200

    Delete client/src/App.test.tsx

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
-
-test('renders the main heading', async () => {
-    render(<App />);
-    const headingElement = await screen.findByText(/Main Heading/i);
-    expect(headingElement).toBeInTheDocument();
-});
