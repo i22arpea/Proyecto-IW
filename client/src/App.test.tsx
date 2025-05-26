59e7d38a14dd06273f17e97d98ba600819823e4c
i22arpea <i22arpea@uco.es>
2025-05-26 22:19:50 +0200

    Revert "Update App.test.tsx"
    This reverts commit 7d62f883ba98338fa7c8d66b006bbfd53d7c3226.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders the main heading', async () => {
+test('renders the main heading', () => {
     render(<App />);
-    const headingElement = await screen.findByText(/Main Heading/i);
+    const headingElement = screen.getByText(/Main Heading/i);
     expect(headingElement).toBeInTheDocument();
 });
