9be5917b64f348c64fbcc42eeea5971c8635c631
i22arpea <114402693+i22arpea@users.noreply.github.com>
2025-05-26 15:37:38 +0200

    Update ProfilePage.tsx

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { desencriptarPalabra } from '../libs/crypto';
 import UserStats from './UserStats'; 
-import { fetchUserStats } from '../utils/fetchUserStats';
+import fetchUserStats from '../utils/fetchUserStats';
 
 
 interface UserProfile {
