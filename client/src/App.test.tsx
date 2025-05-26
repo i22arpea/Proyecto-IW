c77082cc5b355a50c35410f9054642323d142dcf
i22arpea <114402693+i22arpea@users.noreply.github.com>
2025-05-26 15:21:59 +0200

    Update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,9 +2,4 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-describe('Homepage', () => {
-  it('debe mostrar el título Wordle', () => {
-    render(<App />);
-    expect(screen.getByRole('heading', { name: /Wordle/i })).toBeInTheDocument();
-  });
 
