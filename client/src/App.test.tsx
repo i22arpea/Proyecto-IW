7d62f883ba98338fa7c8d66b006bbfd53d7c3226
i22arpea <114402693+i22arpea@users.noreply.github.com>
2025-05-26 15:27:04 +0200

    Update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders the main heading', () => {
+test('renders the main heading', async () => {
     render(<App />);
-    const headingElement = screen.getByText(/Main Heading/i);
+    const headingElement = await screen.findByText(/Main Heading/i);
     expect(headingElement).toBeInTheDocument();
 });
