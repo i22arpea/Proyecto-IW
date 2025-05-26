6fd3e6183212e29d582d22565e9c235dfdce7426
i22arpea <i22arpea@uco.es>
2025-05-26 22:19:55 +0200

    Revert "Update App.test.tsx"
    This reverts commit c77082cc5b355a50c35410f9054642323d142dcf.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,4 +2,9 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
+describe('Homepage', () => {
+  it('debe mostrar el título Wordle', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /Wordle/i })).toBeInTheDocument();
+  });
 
